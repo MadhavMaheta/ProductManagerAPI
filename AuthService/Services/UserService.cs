c49523d2dbe1c052865d50312a869007711374b4
Madhav Maheta <madhavmaheta17@gmail.com>
2024-02-13 16:23:25 +0530

    Password encryption

--- AuthService/Services/UserService.cs
+++ AuthService/Services/UserService.cs
@@ -1,18 +1,20 @@
 ﻿using AuthService.DTO;
 using AuthService.EFCore;
+using AuthService.Helper;
 using AuthService.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage;
 
 namespace AuthService.Services
 {
     public class UserService : IUserService, IDisposable
     {
         public ApplicationDbContext _context { get; set; }
+        private IConfiguration _config;
 
-        public UserService(ApplicationDbContext context)
+        public UserService(ApplicationDbContext context, IConfiguration config)
         {
             _context = context;
+            _config = config;
         }
 
         public void Dispose()
@@ -22,7 +24,11 @@ namespace AuthService.Services
 
         public User GetUser(string username, string password)
         {
-            return _context.User.Where(x => x.Name == username && x.Password == password).FirstOrDefault();
+            return _context.User.Where(x => x.Name == username && x.Password == Encryption.EncryptString(_config.GetSection("EncryptionKey").Value, password)).FirstOrDefault();
+        }
+        public User GetUser(int nUserId)
+        {
+            return _context.User.Where(x => x.Id == nUserId).FirstOrDefault();
         }
 
         public bool RegisterUser(User user)
@@ -41,15 +47,10 @@ namespace AuthService.Services
             }
             objCurrentUser.Name = user.Name;
             objCurrentUser.Email = user.Email;
-            objCurrentUser.Password = user.Password;
+            objCurrentUser.Password = Encryption.EncryptString(_config.GetSection("EncryptionKey").Value, user.Password);
             _context.User.Update(user);
             _context.SaveChanges();
             return true;
-        }
-
-        public User GetUser(int nUserId)
-        {
-            return _context.User.Where(x => x.Id == nUserId).FirstOrDefault();
         }
 
         public bool ChangeUserPassword(UserSetPassword objUserSetPassword)
@@ -57,7 +58,9 @@ namespace AuthService.Services
             User objCurrentUser = _context.User.AsNoTracking().Where(x => x.Id == objUserSetPassword.UserId).FirstOrDefault();
             if (objCurrentUser != null)
             {
-                objCurrentUser.Password = objCurrentUser.Password;
+                objCurrentUser.Password = Encryption.EncryptString(_config.GetSection("EncryptionKey").Value, objUserSetPassword.Password);
+                _context.User.Update(objCurrentUser);
+                _context.SaveChanges();
                 return true;
             }
             else
@@ -65,6 +68,14 @@ namespace AuthService.Services
                 throw new Exception("User not found");
             }
         }
+
+        //public void GetPasswordSetMail(string sUserEmail) {
+        //    User objUser = _context.User.Where(x => x.Email == sUserEmail).FirstOrDefault();
+        //    if(objUser != null)
+        //    {
+
+        //    }
+        //}
 
         public List<User> GetUserList()
         {
